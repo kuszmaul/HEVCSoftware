0507304f006b3b7a9cddb44ccb4951ba470bfbbd
hhi <hhi@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2010-08-16 10:42:26 +0000

    + HHI RQT ROOT

--- source/Lib/TLibCommon/TComDataCU.cpp
+++ source/Lib/TLibCommon/TComDataCU.cpp
@@ -1547,6 +1547,31 @@ UInt TComDataCU::getCtxQtCbf( UInt uiAbsPartIdx, TextType eType, UInt uiTrDepth 
   }
   return uiCtx;
 }
+
+#if HHI_RQT_ROOT
+UInt TComDataCU::getCtxQtRootCbf( UInt uiAbsPartIdx )
+{
+  UInt uiCtx = 0;
+  TComDataCU* pcTempCU;
+  UInt        uiTempPartIdx;
+
+  // Get RootCbf of left PU
+  pcTempCU = getPULeft( uiTempPartIdx, m_uiAbsIdxInLCU + uiAbsPartIdx );
+  if ( pcTempCU )
+  {
+    uiCtx = pcTempCU->getQtRootCbf( uiTempPartIdx );
+  }
+
+  // Get RootCbf of above PU
+  pcTempCU = getPUAbove( uiTempPartIdx, m_uiAbsIdxInLCU + uiAbsPartIdx );
+  if ( pcTempCU )
+  {
+    uiCtx += pcTempCU->getQtRootCbf( uiTempPartIdx ) << 1;
+  }
+
+  return uiCtx;
+}
+#endif
 #endif
 
 UInt TComDataCU::getCtxAlfCtrlFlag( UInt uiAbsPartIdx )
