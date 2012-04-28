5156e2f74fba6bf38d60f09a732b1a64d2e79cfc
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2012-04-28 18:01:04 +0000

    merge long-term picture and enocder restriction on temporal_id changes from HM-6.1-dev into ahg15 branch

--- source/Lib/TLibDecoder/TDecCAVLC.cpp
+++ source/Lib/TLibDecoder/TDecCAVLC.cpp
@@ -1772,6 +1772,7 @@ Void TDecCavlc::parseSliceHeader (TComSlice*& rpcSlice, ParameterSetManagerDecod
             Int decMaxPocLsb = 1<<rpcSlice->getSPS()->getBitsForPOC();
             rps->setPOC(j,rpcSlice->getPOC()-prev-(prevMsb)*decMaxPocLsb); 
             rps->setDeltaPOC(j,-(Int)(prev+(prevMsb)*decMaxPocLsb));
+            rps->setCheckLTMSBPresent(j,true);  
           }
           else
           {
@@ -1779,6 +1780,7 @@ Void TDecCavlc::parseSliceHeader (TComSlice*& rpcSlice, ParameterSetManagerDecod
             rps->setPOC(j,rpcSlice->getPOC()-prev);          
             rps->setDeltaPOC(j,-(Int)prev);
 #if LTRP_MULT
+            rps->setCheckLTMSBPresent(j,false);
           }
           prevDeltaPocLt=prev;
 #endif
