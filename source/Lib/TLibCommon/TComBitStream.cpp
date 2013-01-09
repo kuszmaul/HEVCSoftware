b9d950481a8dd645cf6e191d42ae7153a71b08d0
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2013-01-09 12:49:14 +0000

    ~0 << 32 may be ~0 on some platforms. Avoid this case. See issue #870

--- source/Lib/TLibCommon/TComBitStream.cpp
+++ source/Lib/TLibCommon/TComBitStream.cpp
@@ -98,7 +98,7 @@ void TComOutputBitstream::clear()
 Void TComOutputBitstream::write   ( UInt uiBits, UInt uiNumberOfBits )
 {
   assert( uiNumberOfBits <= 32 );
-  assert( (uiBits & (~0 << uiNumberOfBits)) == 0 );
+  assert( uiNumberOfBits == 32 || (uiBits & (~0 << uiNumberOfBits)) == 0 );
 
   /* any modulo 8 remainder of num_total_bits cannot be written this time,
    * and will be held until next time. */
