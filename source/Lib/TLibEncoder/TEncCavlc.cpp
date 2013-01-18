51ed1b0a9d1ff9776fb9873453d36a0365dd8ed5
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2013-01-18 10:00:16 +0000

    Remove SAVE_BITS_REFPICLIST_MOD_FLAG macro

--- source/Lib/TLibEncoder/TEncCavlc.cpp
+++ source/Lib/TLibEncoder/TEncCavlc.cpp
@@ -884,11 +884,7 @@ Void TEncCavlc::codeSliceHeader         ( TComSlice* pcSlice )
       pcSlice->setNumRefIdx(REF_PIC_LIST_1, 0);
     }
 
-#if SAVE_BITS_REFPICLIST_MOD_FLAG
     if( pcSlice->getPPS()->getListsModificationPresentFlag() && pcSlice->getNumRpsCurrTempList() > 1)
-#else
-    if( pcSlice->getPPS()->getListsModificationPresentFlag() )
-#endif
     {
       TComRefPicListModification* refPicListModification = pcSlice->getRefPicListModification();
       if(!pcSlice->isIntra())
