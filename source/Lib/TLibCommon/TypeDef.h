e0b35074683fb0341d08b1ae12c825ad1e6ef114
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2013-01-25 11:04:48 +0000

    JCTVC-L0232 RD penalty for intra 32x32 in inter slices (encoder only)
    From: Kenneth R. Andersson <kenneth.r.andersson@ericsson.com>

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -53,6 +53,7 @@
 #define L0444_FPA_TYPE             1  ///< remove FPA types 0,1,2 and 7
 #define L0372 1
 #define SIGNAL_BITRATE_PICRATE_IN_VPS               0  ///< K0125: Signal bit_rate and pic_rate in VPS
+#define L0232_RD_PENALTY           1  ///< L0232: RD-penalty for 32x32 TU for intra in non-intra slices
 
 #define MAX_VPS_NUM_HRD_PARAMETERS                1
 #define MAX_VPS_OP_SETS_PLUS1                     1024
