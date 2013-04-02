128f8a1920434a6fb68edd72a0e43bd65b2580af
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2013-04-02 21:29:28 +0000

    Fix for #1053 Patch by: Peisong Chen <peisongc@broadcom.com>

--- source/Lib/TLibCommon/TComSlice.cpp
+++ source/Lib/TLibCommon/TComSlice.cpp
@@ -1234,7 +1234,11 @@ TComVPS::TComVPS()
   for( Int i = 0; i < MAX_TLAYER; i++)
   {
     m_numReorderPics[i] = 0;
+#if L0323_DPB
+    m_uiMaxDecPicBuffering[i] = 1; 
+#else
     m_uiMaxDecPicBuffering[i] = 0; 
+#endif
     m_uiMaxLatencyIncrease[i] = 0;
   }
 }
@@ -1294,7 +1298,11 @@ TComSPS::TComSPS()
   for ( Int i = 0; i < MAX_TLAYER; i++ )
   {
     m_uiMaxLatencyIncrease[i] = 0;
+#if L0323_DPB
+    m_uiMaxDecPicBuffering[i] = 1;
+#else
     m_uiMaxDecPicBuffering[i] = 0;
+#endif
     m_numReorderPics[i]       = 0;
   }
   m_scalingList = new TComScalingList;
