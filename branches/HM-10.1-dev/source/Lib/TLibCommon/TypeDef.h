db331060774be1250beb2b81e8a2a12ad03de0d1
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2013-05-07 09:22:53 +0000

    Remove L0043_TIMING_INFO macro

--- branches/HM-10.1-dev/source/Lib/TLibCommon/TypeDef.h
+++ branches/HM-10.1-dev/source/Lib/TLibCommon/TypeDef.h
@@ -58,7 +58,6 @@
 #define L0045_NON_NESTED_SEI_RESTRICTIONS 1 ///< L0045; Include restriction on the order of APS and non-nested BP, PT and DU info SEI messages
 #define L0044_CPB_DPB_DELAY_OFFSET 1  ///< L0044: Include syntax elements cpb_delay_offset and dpb_delay_offset in the BP SEI message
 #define L0047_APS_FLAGS            1  ///< L0047: Include full_random_access_flag and no_param_set_update_flag in the active parameter set SEI message
-#define L0043_TIMING_INFO          1  ///< L0043: Timing information is signalled in VUI outside hrd_parameters()
 #define L0046_RENAME_PROG_SRC_IDC  1  ///< L0046: Rename progressive_source_idc to source_scan_type
 #define L0045_CONDITION_SIGNALLING 1  ///< L0045: Condition the signaling of some syntax elements in picture timing SEI message
 #define L0043_MSS_IDC 1
