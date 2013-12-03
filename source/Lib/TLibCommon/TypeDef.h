f96d9a40ce9d66da86255653f38505d549d03af3
sony-uk <sony-uk@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2013-12-03 18:06:23 +0000

    RExt: Removed RExt__O0053_O0183_DST_FOR_INTRA_BLOCK_COPY macro
    From: Karl Sharman <karl.sharman@eu.sony.com>

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -247,7 +247,6 @@
 
 #define RExt__ORCE2_A1_GOLOMB_RICE_GROUP_ADAPTATION                            1 ///< 0 = use original HM Golomb-Rice parameter update method, 1 (default) = allow Golomb-Rice parameter for a coefficient group to be based on that of the previous group
 #define RExt__O0044_CU_ADAPTIVE_CHROMA_QP_OFFSET                               1 ///< 0 = use one set of chroma Qp offsets for each slice, 1 (default) = allow multiple sets to be sent and a set selected for each CU
-#define RExt__O0053_O0183_DST_FOR_INTRA_BLOCK_COPY                             1 ///< 0 = use the DST only for non-block-copy intra, 1 (default) = use the DST for both intra and intra-block-copy
 #define RExt__O0067_TRANSFORM_SKIP_SCALING_LIST_RESTRICTION                    1 ///< 0 = allow the use of scaling lists for any TU size, 1 (default) = restrict transform-skip scaling list application to 4x4 only
 #define RExt__O0073_INTRA_BLOCK_COPY_SINGLE_CTX                                1 ///< 0 = use 3 contexts for intra_bc_flag with prediction from left/above, 1 (default) use 1 context for intra_bc_flag without prediction from left/above (NOTE: This is also proposed in JCTVC-O0158 as method 2)
 #define RExt__O0122_INTRA_BLOCK_COPY_PREDICTOR                                 1 ///< 0 = do not use intra block copy predictor, 1 (default) = use intra block copy predictor
