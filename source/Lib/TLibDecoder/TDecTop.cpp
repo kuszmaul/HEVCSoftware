03c2b1c58f903613fd5ac0c08cbe84d6e3ddca77
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2014-03-19 15:15:04 +0000

    Fix issue with compiler warning (Apple LLVM version 5.1 (clang-503.0.38) (based on LLVM 3.4svn))

--- source/Lib/TLibDecoder/TDecTop.cpp
+++ source/Lib/TLibDecoder/TDecTop.cpp
@@ -438,7 +438,7 @@ Bool TDecTop::xDecodeSlice(InputNALUnit &nalu, Int &iSkipFrame, Int iPOCLastDisp
   m_prevSliceSkipped = false;
 
   //we should only get a different poc for a new picture (with CTU address==0)
-  if (m_apcSlicePilot->isNextSlice() && m_apcSlicePilot->getPOC()!=m_prevPOC && !m_bFirstSliceInSequence && (!m_apcSlicePilot->getSliceCurStartCUAddr()==0))
+  if (m_apcSlicePilot->isNextSlice() && m_apcSlicePilot->getPOC()!=m_prevPOC && !m_bFirstSliceInSequence && (m_apcSlicePilot->getSliceCurStartCUAddr()!=0))
   {
     printf ("Warning, the first slice of a picture might have been lost!\n");
   }
