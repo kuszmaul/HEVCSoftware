0b2319336a00a43636565db8494374002754b7f6
karls <karls@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2014-10-09 15:55:05 +0000

    Changed default value of RExt__BACKWARDS_COMPATIBILITY_MOTION_ESTIMATION_R0105 from 1 to 0. This will change the encoder search.

--- branches/HM-dev/source/Lib/TLibCommon/TypeDef.h
+++ branches/HM-dev/source/Lib/TLibCommon/TypeDef.h
@@ -274,7 +274,7 @@
 // NOTE: RExt - Compatibility defaults chosen so that simulations run with the common test conditions do not differ with HM.
 #define RExt__BACKWARDS_COMPATIBILITY_HM_TICKET_1298                           0 ///< Maintain backwards compatibility with HM for ticket 1298 (output value clipping missing from HM RDOQ)
 #define RExt__BACKWARDS_COMPATIBILITY_RBSP_EMULATION_PREVENTION                0 ///< Maintain backwards compatibility with (use same algorithm as) HM for RBSP emulation prevention
-#define RExt__BACKWARDS_COMPATIBILITY_MOTION_ESTIMATION_R0105                  1 ///< Maintain backwards compatibility with HM when coding version 1 profile due to changes in motion estimation introduced in JCTVC-R0105.
+#define RExt__BACKWARDS_COMPATIBILITY_MOTION_ESTIMATION_R0105                  0 ///< Maintain backwards compatibility with HM when coding version 1 profile due to changes in motion estimation introduced in JCTVC-R0105.
 
 //------------------------------------------------
 // Derived macros
