7ce0369073913dd97e038bbe080db7c3aec7e156
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2014-10-13 17:12:10 +0000

    fix compile bug for visual studio 2010

--- branches/HM-dev/source/Lib/TLibCommon/TComBitStream.h
+++ branches/HM-dev/source/Lib/TLibCommon/TComBitStream.h
@@ -85,7 +85,6 @@ class TComOutputBitstream : public TComBitIf
   UInt m_num_held_bits; /// number of bits not flushed to bytestream.
   UChar m_held_bits; /// the bits held and not flushed to bytestream.
                              /// this value is always msb-aligned, bigendian.
-  TComOutputBitstream &operator=(const TComOutputBitstream &); // not defined. do not use
 public:
   // create / destroy
   TComOutputBitstream();
