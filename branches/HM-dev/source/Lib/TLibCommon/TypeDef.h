2ce531b2a11fe9beba503bc08ff7f54afd9cc73a
karls <karls@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2014-10-13 13:18:19 +0000

    Removed macro SEQUENCE_LEVEL_LOSSLESS

--- branches/HM-dev/source/Lib/TLibCommon/TypeDef.h
+++ branches/HM-dev/source/Lib/TLibCommon/TypeDef.h
@@ -126,9 +126,6 @@
 
 #define SBH_THRESHOLD                                     4  ///< I0156: value of the fixed SBH controlling threshold
 
-//NOTE: There is a command-line-controlled cost-mode alternative mechanism - this macro is kept (for now) for backwards-compatibility reasons
-#define SEQUENCE_LEVEL_LOSSLESS                           0  ///< H0530: used only for sequence or frame-level lossless coding
-
 #define DISABLING_CLIP_FOR_BIPREDME                       1  ///< Ticket #175
 
 #define C1FLAG_NUMBER                                     8 // maximum number of largerThan1 flag coded in one chunk :  16 in HM5
