e4ac7b5ae637d89ae9e91ce8f1dedba13d4131c4
binli <binli@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2014-11-06 07:52:27 +0000

    SCC: Integration of S0179 - perform ACT operation on TU blocks. Patch provided by Xiu, Xiaoyu <Xiaoyu.Xiu@InterDigital.com>

--- source/Lib/TLibEncoder/TEncSearch.cpp
+++ source/Lib/TLibEncoder/TEncSearch.cpp
@@ -1209,12 +1209,14 @@ Void TEncSearch::xIntraCodingTUBlock(       TComYuv*    pcOrgYuv,
 #endif
 
   QpParam cQP(*pcCU, compID);
+#if !SCM_S0179_ACT_TU_DEC 
   if(!pcCU->isLosslessCoded(0) && pcCU->getColourTransform(0))
   {
     cQP.Qp = cQP.Qp + (compID==COMPONENT_Cr ? DELTA_QP_FOR_YCgCo_TRANS_V: DELTA_QP_FOR_YCgCo_TRANS);
     cQP.per = cQP.Qp/6;
     cQP.rem= cQP.Qp%6;
   }
+#endif
 
   //===== init availability pattern =====
   Bool  bAboveAvail = false;
@@ -2216,11 +2218,13 @@ TEncSearch::xRecurIntraChromaCodingQT(TComYuv*    pcOrgYuv,
 
     for (UInt ch=COMPONENT_Cb; ch<numberValidComponents; ch++)
     {
+#if !SCM_S0179_ACT_TU_DEC
       if(!pcCU->isLosslessCoded(0) && pcCU->getColourTransform( 0 ))
       {
         m_pcTrQuant->adjustBitDepthandLambdaForColourTrans(ch==COMPONENT_Cr ? DELTA_QP_FOR_YCgCo_TRANS_V: DELTA_QP_FOR_YCgCo_TRANS);
         m_pcRdCost->adjustLambdaForColourTrans            (ch==COMPONENT_Cr ? DELTA_QP_FOR_YCgCo_TRANS_V: DELTA_QP_FOR_YCgCo_TRANS);
       }
+#endif
       const ComponentID compID = ComponentID(ch);
       DEBUG_STRING_NEW(sDebugBestMode)
 
@@ -2333,18 +2337,22 @@ TEncSearch::xRecurIntraChromaCodingQT(TComYuv*    pcOrgYuv,
         DEBUG_STRING_APPEND(sDebug, sDebugBestMode)
         pcCU ->setTransformSkipPartRange                ( bestTransformSkipMode,     compID, subTUAbsPartIdx, partIdxesPerSubTU );
         pcCU ->setCrossComponentPredictionAlphaPartRange( bestCrossCPredictionAlpha, compID, subTUAbsPartIdx, partIdxesPerSubTU );
+#if !SCM_S0179_ACT_TU_DEC
         if(!pcCU->getColourTransform( 0 ))
+#endif
         ruiDist += singleDistC;
       }
       while (TUIterator.nextSection(rTu));
 
       if (splitIntoSubTUs) offsetSubTUCBFs(rTu, compID);
+#if !SCM_S0179_ACT_TU_DEC
       if(!pcCU->isLosslessCoded(0) && pcCU->getColourTransform( 0 ))
       {
         m_pcTrQuant->adjustBitDepthandLambdaForColourTrans(compID==COMPONENT_Cr ? - DELTA_QP_FOR_YCgCo_TRANS_V:  - DELTA_QP_FOR_YCgCo_TRANS );
         m_pcRdCost->adjustLambdaForColourTrans            (compID==COMPONENT_Cr ? - DELTA_QP_FOR_YCgCo_TRANS_V:  - DELTA_QP_FOR_YCgCo_TRANS );
       }
-    } 
+#endif
+    }
   }
   else
   {
@@ -2950,11 +2958,13 @@ TEncSearch::estIntraPredQT(TComDataCU* pcCU,
         }
 
         bMaintainResidual[RESIDUAL_ENCODER_SIDE] = !(m_pcEncCfg->getUseReconBasedCrossCPredictionEstimate());
+#if !SCM_S0179_ACT_TU_DEC
         if(!pcCU->isLosslessCoded(0) && pcCU->getColourTransform( 0 ))
         {
           m_pcTrQuant->adjustBitDepthandLambdaForColourTrans( DELTA_QP_FOR_YCgCo_TRANS );
           m_pcRdCost->adjustLambdaForColourTrans            ( DELTA_QP_FOR_YCgCo_TRANS );
         }
+#endif
 
   // Lambda calculation at equivalent Qp of 4 is recommended because at that Qp, the quantisation divisor is 1.
 #if FULL_NBIT
@@ -3327,11 +3337,13 @@ TEncSearch::estIntraPredQT(TComDataCU* pcCU,
   //===== set distortion (rate and r-d costs are determined later) =====
   ruiDistC                   = uiOverallDistC;
   pcCU->getTotalDistortion() = uiOverallDistY + uiOverallDistC;
+#if !SCM_S0179_ACT_TU_DEC
   if(!pcCU->isLosslessCoded(0) && pcCU->getColourTransform( 0 ))
   {
     m_pcTrQuant->adjustBitDepthandLambdaForColourTrans( -DELTA_QP_FOR_YCgCo_TRANS );
     m_pcRdCost->adjustLambdaForColourTrans            ( -DELTA_QP_FOR_YCgCo_TRANS );
   }
+#endif
 }
 
 Void
