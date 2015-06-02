37b4421855f5ece6e11091c8f409d1ac1fbb7adc
binli <binli@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-06-02 02:18:31 +0000

    SCC: Removed macro SCM_T0063_NUM_PLT_ENTRY

--- branches/HM-SCC-extensions/source/Lib/TLibCommon/TypeDef.h
+++ branches/HM-SCC-extensions/source/Lib/TLibCommon/TypeDef.h
@@ -289,7 +289,6 @@
 //------------------------------------------------
 
 #define SCM_T0227_INTRABC_SIG_UNIFICATION                1 ///< unify intra BC mode and inter mode
-#define SCM_T0063_NUM_PLT_ENTRY                          1 ///< use EG0 to code number of new palette entries
 #define SCM_T0087_IMPROVED_PALETTE_TABLE_GENERATION      1 ///< improved palette table generation
 #define SCM_T0069_AMVR_REFINEMENT                        1 ///< high-level syntax refinement for adaptive motion vector resolution
 #define SCM_T0058_REMOVE_64x64_PLT                       1 ///< Disallow palette mode for 64x64 CUs
