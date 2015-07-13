f4291ea390fed8a25cad2a404e5cfd30395919aa
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-07-13 15:14:38 +0000

    fix for adaptive search range (ASR)

--- branches/HM-dev/source/Lib/TLibEncoder/TEncSlice.cpp
+++ branches/HM-dev/source/Lib/TLibEncoder/TEncSlice.cpp
@@ -479,6 +479,7 @@ Void TEncSlice::resetQP( TComPic* pic, Int sliceQP, Double lambda )
 // Public member functions
 // ====================================================================================================================
 
+//! set adaptive search range based on poc difference
 Void TEncSlice::setSearchRange( TComSlice* pcSlice )
 {
   Int iCurrPOC = pcSlice->getPOC();
@@ -488,9 +489,8 @@ Void TEncSlice::setSearchRange( TComSlice* pcSlice )
   Int iMaxSR = m_pcCfg->getSearchRange();
   Int iNumPredDir = pcSlice->isInterP() ? 1 : 2;
 
-  for (Int iDir = 0; iDir <= iNumPredDir; iDir++)
+  for (Int iDir = 0; iDir < iNumPredDir; iDir++)
   {
-    //RefPicList e = (RefPicList)iDir;
     RefPicList  e = ( iDir ? REF_PIC_LIST_1 : REF_PIC_LIST_0 );
     for (Int iRefIdx = 0; iRefIdx < pcSlice->getNumRefIdx(e); iRefIdx++)
     {
