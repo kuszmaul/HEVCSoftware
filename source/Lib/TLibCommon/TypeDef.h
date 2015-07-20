40fcd1797a91fbdbbb4e5a3763cb2c05ef49d4b1
rapaka <rapaka@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-07-20 21:17:32 +0000

    SCC : Integration of JCTVC-U0087 : Swap the nesting of the loops for sending the escape-coded entries. Patch from <wpu@qti.qualcomm.com>

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -194,6 +194,7 @@
 #define SCM_U0133_REORDER                                1 ///< JCTVC-U0133: change the positions of palette scanRotationModeFlag and palette delta qp and chroma qp offset
 #define SCM_U0081_AMVR_UNIFICATION                       1 ///< JCTVC-U0081 : AMVR unification
 #define SCM_IBC_CR_INTERPOLATION_ENABLE                  1 ///< JCTVC-O0080/77/103 : Enable IBC chroma interpolation
+#define SCM_U0087_SWAP_ESC_ORDER                         1 ///< JCTVC-U0087 : Swap the nesting of the the loops for sending the escape-coded entries so that they are sent in component-grouped order (U0087 approach #2)
 
 //------------------------------------------------
 // Derived macros
