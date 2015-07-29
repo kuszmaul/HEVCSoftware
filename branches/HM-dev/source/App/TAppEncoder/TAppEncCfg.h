83ae9af483df630fc35079db7369f97581e28624
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-07-29 14:22:34 +0000

    Add option to add Access Unit Delimiters

--- branches/HM-dev/source/App/TAppEncoder/TAppEncCfg.h
+++ branches/HM-dev/source/App/TAppEncoder/TAppEncCfg.h
@@ -84,6 +84,7 @@ protected:
   Int       m_confWinBottom;
   Int       m_framesToBeEncoded;                              ///< number of encoded frames
   Int       m_aiPad[2];                                       ///< number of padded pixels for width and height
+  Bool      m_AccessUnitDelimiter;                            ///< add Access Unit Delimiter NAL units
   InputColourSpaceConversion m_inputColourSpaceConvert;       ///< colour space conversion to apply to input video
   Bool      m_snrInternalColourSpace;                       ///< if true, then no colour space conversion is applied for snr calculation, otherwise inverse of input is applied.
   Bool      m_outputInternalColourSpace;                    ///< if true, then no colour space conversion is applied for reconstructed video, otherwise inverse of input is applied.
