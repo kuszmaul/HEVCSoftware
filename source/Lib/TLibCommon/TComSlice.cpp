28efb4ca8ce95dba48470d83dc4ca2c09088c4f7
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-08-21 17:30:06 +0000

    fix naming of MaxLatencyIncreasePlus1

--- source/Lib/TLibCommon/TComSlice.cpp
+++ source/Lib/TLibCommon/TComSlice.cpp
@@ -1549,7 +1549,7 @@ TComSPS::TComSPS()
 
   for ( Int i = 0; i < MAX_TLAYER; i++ )
   {
-    m_uiMaxLatencyIncrease[i] = 0;
+    m_uiMaxLatencyIncreasePlus1[i] = 0;
     m_uiMaxDecPicBuffering[i] = 1;
     m_numReorderPics[i]       = 0;
   }
