2cd4a2ec989c3f7c18f172b22f99b0e9be143273
rapaka <rapaka@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-09-26 00:33:58 +0000

    SCC : Bug Fix for Ticket 1417

--- branches/HM-SCC-extensions/source/Lib/TLibCommon/TComPrediction.cpp
+++ branches/HM-SCC-extensions/source/Lib/TLibCommon/TComPrediction.cpp
@@ -535,7 +535,11 @@ Void TComPrediction::predIntraAng( const ComponentID compID, UInt uiDirMode, Pel
 #endif
       xPredIntraAng( channelsBitDepthForPrediction, ptrSrc+sw+1, sw, pDst, uiStride, iWidth, iHeight, channelType, uiDirMode, enableEdgeFilters );
       
+#if SCM_FIX_DC_FILT_TICKET_1417
+      if( uiDirMode == DC_IDX && !pcCU->getSlice()->getSPS()->getSpsScreenExtension().getDisableIntraBoundaryFilter() )
+#else
       if( uiDirMode == DC_IDX && enableEdgeFilters )
+#endif 
       {
         xDCPredFiltering( ptrSrc+sw+1, sw, pDst, uiStride, iWidth, iHeight, channelType );
       }
