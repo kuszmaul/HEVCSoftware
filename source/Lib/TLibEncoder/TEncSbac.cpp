88a62a82e9955fff9d1536813b4c96c5ea65f3aa
binli <binli@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-10-16 06:23:30 +0000

    SCC: Removed macro SCM_FIX_DELTA_QP_SIGN_TICKET_1421.

--- source/Lib/TLibEncoder/TEncSbac.cpp
+++ source/Lib/TLibEncoder/TEncSbac.cpp
@@ -727,13 +727,6 @@ Void TEncSbac::codePLTModeSyntax(TComDataCU* pcCU, UInt uiAbsPartIdx, UInt uiNum
       codeChromaQpAdjustment( pcCU, uiAbsPartIdx );
       *codeChromaQpAdj = false;
     }
-
-#if !SCM_FIX_DELTA_QP_SIGN_TICKET_1421
-    for (Int comp = compBegin; comp < compBegin + uiNumComp; comp++)
-    {
-      uiMaxVal[comp] = pcCU->xCalcMaxVals(pcCU, ComponentID(comp));
-    }
-#endif
   }
 #endif
 
