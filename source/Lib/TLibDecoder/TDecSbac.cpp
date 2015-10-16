a0b4b90a2039166a1c7f8a69064a91bfb96525ac
binli <binli@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2015-10-16 05:14:33 +0000

    SCC: Removed macro SCM_U0133_REORDER.

--- source/Lib/TLibDecoder/TDecSbac.cpp
+++ source/Lib/TLibDecoder/TDecSbac.cpp
@@ -746,44 +746,11 @@ Void TDecSbac::parsePLTModeSyntax(TComDataCU *pcCU, UInt uiAbsPartIdx, UInt uiDe
     if (paletteEscapeValPresentFlag)
     {
       uiSignalEscape = 1;
-
-#if SCM_S0043_PLT_DELTA_QP  && !SCM_U0133_REORDER
-      if( pcCU->getSlice()->getPPS()->getUseDQP() && bCodeDQP )
-      {
-        parseDeltaQP( pcCU, uiAbsPartIdx, pcCU->getDepth( uiAbsPartIdx ) );
-        bCodeDQP = false;
-      }
-
-      if( pcCU->getSlice()->getUseChromaQpAdj() && codeChromaQpAdj )
-      {
-        parseChromaQpAdjustment( pcCU, uiAbsPartIdx, pcCU->getDepth( uiAbsPartIdx ) );
-        codeChromaQpAdj = false;
-      }
-
-      for (Int comp = compBegin; comp < compBegin + uiNumComp; comp++)
-      {
-        uiMaxVal[comp] = pcCU->xCalcMaxVals(pcCU, ComponentID(comp));
-      }
-#endif
     }
     else
     {
       uiSignalEscape = 0;
     }
-#if !SCM_U0133_REORDER
-    if (uiDictMaxSize + uiSignalEscape > 1)
-    {
-      parseScanRotationModeFlag(pcCU, uiAbsPartIdx, uiDepth);
-    }
-    else
-    {
-      pcCU->setPLTScanRotationModeFlagSubParts(false, uiAbsPartIdx, uiDepth);
-    }
-  }
-  else
-  {
-    pcCU->setPLTScanRotationModeFlagSubParts(false, uiAbsPartIdx, uiDepth);
-#endif
   }
 
   UInt uiDictIdxBitsExteneded = uiDictIdxBits;
@@ -816,19 +783,15 @@ Void TDecSbac::parsePLTModeSyntax(TComDataCU *pcCU, UInt uiAbsPartIdx, UInt uiDe
       lParsedIdxList.push_back(uiSymbol);
     }
     m_pcTDecBinIf->decodeBin(lastRunType, m_SPointSCModel.get(0, 0, 0) RExt__DECODER_DEBUG_BIT_STATISTICS_PASS_OPT_ARG(STATS__CABAC_DICTIONARY_BITS));
-#if SCM_U0133_REORDER
     parseScanRotationModeFlag(pcCU, uiAbsPartIdx, uiDepth);
-#endif
     uiAdjust = 0;
   }
-#if SCM_U0133_REORDER
   else
   {
     pcCU->setPLTScanRotationModeFlagSubParts(false, uiAbsPartIdx, uiDepth);
   }
-#endif
 
-#if SCM_S0043_PLT_DELTA_QP && SCM_U0133_REORDER
+#if SCM_S0043_PLT_DELTA_QP
   if ( uiSignalEscape )
   {
     if( pcCU->getSlice()->getPPS()->getUseDQP() && bCodeDQP )
